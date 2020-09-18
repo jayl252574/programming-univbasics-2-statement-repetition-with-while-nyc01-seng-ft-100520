e8be20c99ccd5f2758c1eb00c83ca4e6a4c792c2
Jay Liu <jayl252574@gmail.com>
2020-09-18 14:39:25 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,13 @@
-# Write your code here
+expect{ load file }.to output(/10/).to_stdout
+  expect{ load file }.to output(/9/).to_stdout
+  expect{ load file }.to output(/8/).to_stdout
+  expect{ load file }.to output(/7/).to_stdout
+  expect{ load file }.to output(/6/).to_stdout
+  expect{ load file }.to output(/5/).to_stdout
+  expect{ load file }.to output(/4/).to_stdout
+  expect{ load file }.to output(/3/).to_stdout
+  expect{ load file }.to output(/2/).to_stdout
+  expect{ load file }.to output(/1/).to_stdout
+
+  expect{ load file }.to output(/10(\r\n|\r|\n)9(\r\n|\r|\n)8(\r\n|\r|\n)7(\r\n|\r|\n)6(\r\n|\r|\n)5(\r\n|\r|\n)4(\r\n|\r|\n)3(\r\n|\r|\n)2(\r\n|\r|\n)1/).to_stdout, "expected:\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1\n\nEach number should be on a new line and in the correct order."
+end
